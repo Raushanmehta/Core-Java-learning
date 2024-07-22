381ed51efcc61076ec89ed56bfb1a09990dab0d6
Raushan Mehta <raushanmehta2184@gmail.com>
2024-07-23 00:36:23 +0530

    inheritance

--- src/querySolve10.java
+++ src/querySolve10.java
@@ -1,5 +1,30 @@
+class Circles {
+    public int radius;
+    Circles(int r) {
+        System.out.println("I am circle radius");
+        this.radius = r;
+    }
+    public double area() {
+        return Math.PI * this.radius * this.radius;
+    }
+}
+
+class Cylinder1 extends Circles {
+    public int height;
+    Cylinder1(int r, int h) {
+        super(r);
+        System.out.println("I am cylinder in construction");
+        this.height = h;
+    }
+    public double volume() {
+        return Math.PI * this.radius * this.radius * this.height;
+    }
+}
+
 public class querySolve10 {
     public static void main(String[] args) {
-        
+        // Problem 1
+        Cylinder1 sc = new Cylinder1(5, 8);
+
     }
 }
