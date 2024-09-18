957739dd159fe568360670c7240501cfaa135aaf
Raushan Mehta <raushanmehta2184@gmail.com>
2024-09-18 15:28:25 +0530

    adv java

---
+++ src/calculator.java
@@ -0,0 +1,16 @@
+public class calculator {
+    public static void main(String[] args) {
+        /*
+        You have to create a custom a calculator with following operation;
+        1. + -> Addition
+        2. - -> Subtraction
+        3. / -> Division
+        4. * -> Multiplication
+        Which throw following Exceptions
+        1. Invalid input Exception ex: 8 & 9
+        2. Cannot divide by 0 Exception
+        3. Max Input Exception if any of the input is grater than 100000
+        4. Max multiplier reached Exception  - Don't allow any multiplication input to be greater than 7000
+        */
+    }
+}
--- src/querySolve14.java
+++ src/querySolve14.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class querySolve14 {
     public static void main(String[] args) {
 
@@ -9,12 +11,32 @@ public class querySolve14 {
         //System.out.println(yearBone);
 
         //Problem 2
-        try {
+        /*try {
             int h = 99/6;
             System.out.println("Hehe");
         }catch (ArithmeticException e){
             System.out.println("Haha");
-        }
+        }*/
 
+        //Problem 3
+        /*boolean flag = true;
+        int [] marks = new int[3];
+        marks[0] = 15;
+        marks[1] = 45;
+        marks[2] = 95;
+        Scanner sc = new Scanner(System.in);
+        int index;
+        int i = 0;
+        while (flag && i<5){
+         try {
+             System.out.println("Enter index Number");
+             index = sc.nextByte();
+             System.out.println("The value of marks[index] is: " + marks[index]);
+             break;
+         }catch (ArrayIndexOutOfBoundsException e){
+             System.out.println("index is not exist");
+             i++;
+
+        }*/
     }
 }
