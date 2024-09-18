3336a4d95c91d7f0718a294f0a201ea089311457
Raushan Mehta <raushanmehta2184@gmail.com>
2024-09-18 12:49:35 +0530

    solveQ

---
+++ src/querySolve14.java
@@ -0,0 +1,20 @@
+public class querySolve14 {
+    public static void main(String[] args) {
+
+        // Problem 1
+        // syntax error   int  a = 9
+        // Logical error
+        //int a = 65;
+        //int yearBone = 2000-65;
+        //System.out.println(yearBone);
+
+        //Problem 2
+        try {
+            int h = 99/6;
+            System.out.println("Hehe");
+        }catch (ArithmeticException e){
+            System.out.println("Haha");
+        }
+
+    }
+}
